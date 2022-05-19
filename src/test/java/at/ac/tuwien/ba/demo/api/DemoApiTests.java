8760ad6c7fc6f77c3941c5a081717ad0ff5c9e8d
Martin Hungendorfer <e11904212@student.tuwien.ac.at>
2022-05-19 14:31:55 +0200

    init

---
+++ src/test/java/at/ac/tuwien/ba/demo/api/DemoApiTests.java
@@ -0,0 +1,13 @@
+package at.ac.tuwien.ba.demo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApiTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
